7d17afce0f9d63aaa259ca00069670625b16a98b
Flavius Alecu <flaviuso@hotmail.com>
2011-06-21 23:56:48 +0100

    added caching of candidate search files into a zlib compressed package. this greatly increases search speeds. currently supports only a single search directory and cache will have to manually be rebuilt when source files get updated

--- fex.cpp
+++ fex.cpp
@@ -6,8 +6,14 @@
 #include <util/util.h>
 #include <util/logging.h>
 
+#include "zlib/zlib.h"
+
 // must include after re2 stuff
 #include <windows.h>
+
+#include <fcntl.h>
+#include <io.h>
+#define SET_BINARY_MODE(file) _setmode(_fileno(file), O_BINARY)
 
 struct sFile
 {
@@ -17,26 +23,34 @@ struct sFile
 
 struct sBufferFile
 {
-	std::string name;
-	const char* ptr;
+	char name[256];
+	unsigned int ptrOffset;
 };
 
 std::vector<std::string> filters;
 std::vector<sFile> files;
-std::vector<sBufferFile> bufferFiles;
 
 char searchPath[256];
 char folderPath[256];
+char cachePath[256];
 std::vector<std::string> folders;
 
 char* megaBuffer = NULL;
 unsigned int megaBufferSize = 0;
+
+unsigned int numBufferFiles = 0;
+sBufferFile* bufferFiles = NULL;
+
+char* globalBuffer = NULL;
+unsigned int globalBufferSize = 0;
 
 const int DISPLAY_LINE_LENGTH = 128;
 char displayLine[DISPLAY_LINE_LENGTH] = {0};
 
 char buildFilter[256];
 bool recursive = false;
+bool cache = false;
+bool recache = false;
 
 const char* GetExtension(const char* filename)
 {
@@ -51,8 +65,95 @@ const char* GetExtension(const char* filename)
 	return NULL;
 }
 
+void CleanUp()
+{
+	delete globalBuffer;
+	globalBuffer = NULL;
+	globalBufferSize = 0;
+	megaBuffer = NULL;
+	bufferFiles = NULL;
+	numBufferFiles = 0;
+}
+
 void BuildPackage()
 {
+	bool foundCache = false;
+
+	std::string currentFolder = folders.back();
+	strcpy_s(cachePath, currentFolder.c_str());
+	strcat_s(cachePath, "\\.fex_cache");
+
+	if (cache && !recache)
+	{
+		// TODO: handle several directories
+
+		FILE* fp = fopen(cachePath, "r");
+		if (fp)
+		{
+			SET_BINARY_MODE(fp);
+
+			fseek(fp, 0L, SEEK_END);
+			int size = ftell(fp);
+			fseek(fp, 0L, SEEK_SET);
+
+			char* inBuffer = new char[size];
+			if (fread(inBuffer, 1, size, fp) != size && ferror(fp))
+			{
+				printf("fex warning: couldn't read from file %s (%d)\n", cachePath, ferror(fp));
+				fclose(fp);
+				delete inBuffer;
+				goto fallback;
+			}
+			fclose(fp);
+			
+			z_stream stream;
+			stream.zalloc = Z_NULL;
+			stream.zfree = Z_NULL;
+			stream.opaque = Z_NULL;
+			stream.avail_in = size - sizeof(int);
+			stream.next_in = (Bytef*)(inBuffer + sizeof(int));
+
+			int ret = inflateInit(&stream);
+			if (ret != Z_OK)
+			{
+				printf("fex warning: couldn't init zlib for uncompression (%d)\n", ret);
+				delete inBuffer;
+				goto fallback;
+			}
+
+			globalBufferSize = *(int*)(inBuffer);
+			globalBuffer = new char[globalBufferSize];
+
+			stream.avail_out = globalBufferSize;
+			stream.next_out = (Bytef*)globalBuffer;
+			ret = inflate(&stream, Z_FINISH);
+			if (ret != Z_STREAM_END)
+			{
+				printf("fex warning: couldn't uncompress cache (%d)\n", ret);
+				delete inBuffer;
+				delete globalBuffer;
+				megaBufferSize = 0;
+				goto fallback;
+			}
+
+			// fixup pointers
+			numBufferFiles = *(int*)globalBuffer;							// first word is number of files in bufferFiles
+			bufferFiles = (sBufferFile*)(globalBuffer + sizeof(int));		// next chunk is the bufferFiles buffer
+			megaBuffer = (char*)(bufferFiles + numBufferFiles);				// the rest is the mega buffer with the text
+			megaBufferSize = globalBufferSize - (sizeof(int) + sizeof(sBufferFile) * numBufferFiles);
+
+			inflateEnd(&stream);
+			delete inBuffer;
+			foundCache = true;
+		}
+	}
+
+	if (foundCache)
+	{
+		return;
+	}
+
+fallback:
 	OSVERSIONINFO osInfo;
 	osInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
 	GetVersionEx(&osInfo);
@@ -140,26 +241,33 @@ void BuildPackage()
 
 	//printf("BuildPackage: found %d files...total of %.2f Mb\n", numFiles, size.QuadPart / 1024.f / 1024.f);
 
-	delete megaBuffer;
-	megaBuffer = NULL;
-
 	if (numFiles == 0)
 	{
 		return;
 	}
 
-	bufferFiles.reserve(numFiles);
+	CleanUp();
 
 	megaBufferSize = (unsigned int)size.QuadPart + (1024 * 1024); // an extra meg as safety net
-	megaBuffer = new char[megaBufferSize];
+
+	globalBufferSize = sizeof(sBufferFile) * numFiles;
+	globalBufferSize += megaBufferSize;
+
+	// allocate global buffer, store sizes and fixup pointers
+	globalBuffer = new char[globalBufferSize];
+	*(int*)globalBuffer = numFiles;									// first word is number of files in bufferFiles
+	bufferFiles = (sBufferFile*)(globalBuffer + sizeof(int));		// next chunk is the bufferFiles buffer
+	megaBuffer = (char*)(bufferFiles + numFiles);					// the rest is the mega buffer with the text
+
 	char* put = megaBuffer;
-	if (!megaBuffer)
+	if (!globalBuffer)
 	{
 		printf("fex error: out of memory!\n");
 		return;
 	}
 
 	// read all files into memory...yes
+	unsigned int bfIdx = 0;
 	for (unsigned int i = 0; i < files.size(); ++i)
 	{
 		const sFile& file = files[i];
@@ -175,17 +283,58 @@ void BuildPackage()
 		put += file.size;
 		fclose(fp);
 
-		sBufferFile bufferFile;
-		bufferFile.name = file.name;
-		bufferFile.ptr = put;
-		bufferFiles.push_back(bufferFile);
+		sprintf_s(bufferFiles[bfIdx].name, "%s", file.name.c_str());
+		bufferFiles[bfIdx].ptrOffset = put - megaBuffer;
+		bfIdx++;
 	}
-}
+	numBufferFiles = bfIdx;
 
-void CleanUp()
-{
-	delete megaBuffer;
-	megaBuffer = NULL;
+	if (cache || recache)
+	{
+		// TODO: handle several directories
+
+		FILE* fp = fopen(cachePath, "w");
+		if (fp)
+		{
+			SET_BINARY_MODE(fp);
+			
+			z_stream stream;
+			stream.zalloc = Z_NULL;
+			stream.zfree = Z_NULL;
+			stream.opaque = Z_NULL;
+
+			int ret = deflateInit(&stream, Z_DEFAULT_COMPRESSION);
+			if (ret != Z_OK)
+			{
+				printf("fex warning: couldn't init zlib for compression (%d)\n", ret);
+				return;
+			}
+
+			char* outBuffer = new char[globalBufferSize];
+
+			stream.avail_in = globalBufferSize;
+			stream.next_in = (Bytef*)globalBuffer;
+			stream.avail_out = globalBufferSize - sizeof(int);
+			stream.next_out = (Bytef*)(outBuffer + sizeof(int));
+			ret = deflate(&stream, Z_FINISH);
+			if (ret != Z_STREAM_END)
+			{
+				printf("fex warning: couldn't decompress cache (%d)\n", ret);
+				delete outBuffer;
+				return;
+			}
+			deflateEnd(&stream);
+
+			*(int*)outBuffer = globalBufferSize;
+
+			if (fwrite(outBuffer, 1, stream.total_out + sizeof(int), fp) != stream.total_out + sizeof(int) && ferror(fp))
+			{
+				printf("fex warning: couldn't write to file %s (%d)\n", searchPath, ferror(fp));
+				delete outBuffer;
+			}
+			fclose(fp);
+		}
+	}
 }
 
 void ParseFilters(const char* cFilter)
@@ -221,6 +370,8 @@ void ShowHelp()
 	printf("\t -e\t A list of file extensions to include in the search,\n\t\t separated by space or comma.\n\t\t e.g. '-e \"c, cpp, h\"'\n\n");
 	printf("\t -d\t The root directory of the search.\n\t\t e.g. '-d \"c:/my_code\"'\n\n");
 	printf("\t -r\t Search recursively through sub folders.\n\n");
+	printf("\t -c\t Use the cache for all searches. If no cache is found, create on in the search directory.\n\n");
+	printf("\t -C\t Re-cache all found source files in the search dir. Use this flag to renew the cache.\n\n");
 
 	printf("pattern:\n");
 	printf("\t For supported patterns have a look at\n\t code.google.com/p/re2/wiki/Syntax\n");
@@ -258,6 +409,12 @@ void main(int argc, const char* argv[])
 		case 'r':
 			recursive = true;
 			break;
+		case 'c':
+			cache = true;
+			break;
+		case 'C':
+			recache = true;
+			break;
 		}
 	}
 
@@ -279,13 +436,17 @@ void main(int argc, const char* argv[])
 		return;
 	}
 
+	//LARGE_INTEGER start1, stop1, start2, stop2;
+	//QueryPerformanceCounter(&start1);
+
 	// build our data
 	BuildPackage();
 
+	//QueryPerformanceCounter(&stop1);
+
 	if (megaBuffer)
 	{
-		//LARGE_INTEGER start, stop;
-		//QueryPerformanceCounter(&start);
+		//QueryPerformanceCounter(&start2);
 
 		RE2 mainSearch(argv[argc - 1]);
 		RE2 newLineSearch("\n");
@@ -299,9 +460,9 @@ void main(int argc, const char* argv[])
 		while (RE2::FindAndConsume(&megaString, mainSearch))
 		{
 			// identify which file the search hit was in
-			for (unsigned int i = lastFound; i < bufferFiles.size(); ++i)
+			for (unsigned int i = lastFound; i < numBufferFiles; ++i)
 			{
-				if (megaString.data() <= bufferFiles[i].ptr)
+				if (megaString.data() <= megaBuffer + bufferFiles[i].ptrOffset)
 				{
 					// do a regexp search to find all newlines for this file up to the point of the hit
 					// if previous search had a hit on the same file, use the cached numNewLines and ptr so
@@ -315,7 +476,7 @@ void main(int argc, const char* argv[])
 					}
 					else
 					{
-						beg = i == 0 ? megaBuffer : bufferFiles[i - 1].ptr;
+						beg = i == 0 ? megaBuffer : megaBuffer + bufferFiles[i - 1].ptrOffset;
 					}
 
 					const char* end = megaString.data();
@@ -345,7 +506,7 @@ void main(int argc, const char* argv[])
 							break;
 						}
 					}
-					printf("%s:%d:%s\n", bufferFiles[i].name.c_str(), numNewLines + 1, displayLine);
+					printf("%s:%d:%s\n", bufferFiles[i].name, numNewLines + 1, displayLine);
 					lastFound = i;
 					break;
 				}
@@ -353,8 +514,9 @@ void main(int argc, const char* argv[])
 			num++;
 		}
 
-		//QueryPerformanceCounter(&stop);
-		//printf("fex: done in: %.2f seconds, found %d instances\n", (stop.QuadPart - start.QuadPart) / 1000000.f, num);
+		//QueryPerformanceCounter(&stop2);
+		//printf("fex: data accumulation done in: %.2f seconds, %d files searched\n", (stop1.QuadPart - start1.QuadPart) / 1000000.f, numBufferFiles);
+		//printf("fex: regexp match done in: %.2f seconds, found %d instances\n", (stop2.QuadPart - start2.QuadPart) / 1000000.f, num);
 	}
 
 	CleanUp();
